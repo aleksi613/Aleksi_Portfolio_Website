1105c86b4a4a703ebb35dac8894bcb18e717cd83
Aleksi Valioghli <145508040+aleksi613@users.noreply.github.com>
2025-01-16 03:55:43 -0500

    Fixed to properly configure the dist folder (for hosting)

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,33 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
+import { resolve } from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'src/assets/models/**/*',
+          dest: 'assets/models'
+        }
+      ]
+    })
+  ],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+      },
+      output: {
+        assetFileNames: (assetInfo) => {
+          if (/\.gltf$/.test(assetInfo.name)) {
+            return 'assets/models/[name][extname]';
+          }
+          return 'assets/[name][extname]';
+        },
+      },
+    },
+  },
+});
